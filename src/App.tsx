5b2117a2d97903c496a4add1ea9dd9624160677f
Christopher Little <christopher.little.personal@gmail.com>
2025-01-06 05:50:11 +0000

    Checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import Profile from './components/Profile';
-import Education from './components/Education';
-import Skills from './components/Skills';
-import Experience from './components/Experience';
-import data from '../data.json';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div className="container mx-auto p-4">
-      <Profile 
-        name={data.profile.name} 
-        title={data.profile.title} 
-        email={data.profile.contact.email} 
-        phone={data.profile.contact.phone} 
-      />
-      <Education education={data.education} />
-      <Skills skills={data.skills} />
-      <Experience experience={data.experience} />
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
-export default App; 
+export default App;
